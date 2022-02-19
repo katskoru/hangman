735397ddd8bbb99dfd499ffb571f6bf894f17068
katskoru <katskoru@gmail.com>
2022-02-19 14:41:11 +0100

    menu

--- lib/home/screens/home.dart
+++ lib/home/screens/home.dart
@@ -6,8 +6,36 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title: Text("HangMan Game"),
+        centerTitle: true,
+        title: Text(
+          "HangMan Game",
+          style: TextStyle(fontFamily: "Marker", fontSize: 30.0),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("New Game"),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20.0),
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text("LeaderBoard"),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Log Out"),
+              style: ElevatedButton.styleFrom(primary: Colors.red[900]),
+            )
+          ],
+        ),
       ),
     );
   }
