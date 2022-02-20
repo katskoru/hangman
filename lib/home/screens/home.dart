8422917dfc4d82174b579cc801b9c9f279fcca9a
katskoru <katskoru@gmail.com>
2022-02-20 11:47:23 +0100

    NewGameProvider

--- lib/home/screens/home.dart
+++ lib/home/screens/home.dart
@@ -1,48 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:hang/newGame/data/providers/new_game_provider.dart';
 import 'package:hang/newGame/screens/new_game_screen.dart';
 import 'package:hang/widgets/text_widget.dart';
+import 'package:provider/provider.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[900],
-      appBar: AppBar(
-          centerTitle: true,
-          title: const MyTextWidget(
-            text: "HangMan",
-            size: 30.0,
-          )),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              "assets/img/pexels-arindam-raha-2213575.jpg",
-              width: 70.0,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => NewGame()));
-              },
-              child: const Text("New Game"),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20.0),
-              child: ElevatedButton(
-                onPressed: () {},
-                child: const Text("Leaderboard"),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => NewGameProvider())
+      ],
+      child: Scaffold(
+        backgroundColor: Colors.grey[900],
+        appBar: AppBar(
+            centerTitle: true,
+            title: const MyTextWidget(
+              text: "HangMan",
+              size: 30.0,
+            )),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                "assets/img/pexels-arindam-raha-2213575.jpg",
+                width: 70.0,
               ),
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text("Log Out"),
-              style: ElevatedButton.styleFrom(primary: Colors.red[900]),
-            )
-          ],
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => NewGame()));
+                },
+                child: const Text("New Game"),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text("Leaderboard"),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text("Log Out"),
+                style: ElevatedButton.styleFrom(primary: Colors.red[900]),
+              )
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/newGame/data/providers/new_game_provider.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/foundation.dart';
+
+class NewGameProvider extends ChangeNotifier {
+  final List _alphabet = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z'
+  ];
+  List get alphabet => _alphabet;
+}
