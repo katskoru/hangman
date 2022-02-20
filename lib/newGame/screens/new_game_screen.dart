adee00ae6055323d80f741ca4f095f6ee2632eaf
katskoru <katskoru@gmail.com>
2022-02-20 10:50:23 +0100

    newgame

---
+++ lib/newGame/screens/new_game_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NewGame extends StatelessWidget {
+  const NewGame({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
