018f3e0d95ba1a9c12aad9abf670ee6d73ad7cd4
katskoru <katskoru@gmail.com>
2022-02-27 11:58:14 +0100

    timer

--- lib/newGame/screens/new_game_screen.dart
+++ lib/newGame/screens/new_game_screen.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:hang/newGame/data/providers/new_game_provider.dart';
+import 'package:hang/newGame/data/providers/timer_provider.dart';
 import 'package:hang/newGame/screens/new_game_body.dart';
 import 'package:hang/widgets/text_widget.dart';
 import 'package:provider/provider.dart';
 
-class NewGame extends StatelessWidget {
+class NewGame extends StatefulWidget {
   const NewGame({Key? key}) : super(key: key);
+
+  @override
+  State<NewGame> createState() => _NewGameState();
+}
+
+class _NewGameState extends State<NewGame> {
+  @override
+  void initState() {
+    // Provider.of<TimerProvider>(context).init();
+    context.read<TimerProvider>().init();
+    super.initState();
+
+    @override
+    void dispose() {
+      super.dispose();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     int? _currentWord = Provider.of<NewGameProvider>(context).currentWord;
+
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
@@ -18,9 +37,10 @@ class NewGame extends StatelessWidget {
             },
             icon: const Icon(Icons.reply_outlined)),
         centerTitle: true,
-        title: const MyTextWidget(
+        title: MyTextWidget(
           size: 30.0,
-          text: "Timer",
+          text:
+              Provider.of<TimerProvider>(context).timer!.tick.toString() + "s",
         ),
         actions: [
           Padding(
